657b1e98166d811e2539611967fa250eb24c0ace
adambournes21 <adam.bournes2003@gmail.com>
2023-09-19 16:06:22 -0400

    name fixed

--- utils/QueryProcessor.tsx
+++ utils/QueryProcessor.tsx
@@ -1,5 +1,34 @@
 export default function QueryProcessor(query: string): string {
 
+  if (query.toLowerCase().includes("Which of the following numbers is both a square and a cube:")) {
+    const matches = query.match(/\d+/g);
+    // Convert the matches (which are strings) to actual numbers
+    const numbers = matches ? matches.map(Number) : [];
+    function isPerfectSquare(n) {
+      return Math.sqrt(n) % 1 === 0;
+    }
+  
+    function isPerfectCube(n) {
+        return Math.cbrt(n) % 1 === 0;
+    }
+    
+    function numbersThatAreBothSquareAndCube(inputStr) {
+        const matches = inputStr.match(/\d+/g);
+        if (!matches) return [];
+    
+        return matches.map(Number).filter(num => {
+            for (let i = 1; i <= 18; i++) {
+                if (num === i * i && num === i * i * i) {
+                    return true;
+                }
+            }
+            return false;
+        });
+    }
+
+    return (Math.max(...numbers)).toString();
+  }
+
   if (query.toLowerCase().includes("which of the following numbers is the largest:")) {
     const matches = query.match(/\d+/g);
     // Convert the matches (which are strings) to actual numbers
@@ -15,7 +44,7 @@ export default function QueryProcessor(query: string): string {
 
   if (query.toLowerCase().includes("name")) {
     return (
-      "Your name is Adam"
+      "Rice Cracker"
     );
   }
 
